b7b6495c77f2d0b90f28f48a77b35336a04268b7
Chai Landau <clandau@cliquestudios.com>
2022-03-29 09:57:19 -0400

    starts mobile menu

--- components/Header/Menu.js
+++ components/Header/Menu.js
@@ -1,81 +1,95 @@
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 const Menu = () => {
+  const [ hamburgerOpen, setHamburgerOpen ] = useState( false );
+
+  const toggleHamburger = () => {
+    setHamburgerOpen( !hamburgerOpen );
+    console.log( "toggle" );
+  };
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link
-            to="whiskey"
-            smooth={true}
-            offset={-100}
-            duration={750}
-            hashSpy={true}>
+    <>
+      <button className="menu-toggle" onClick={toggleHamburger}>
+        <span className="top line"></span>
+        <span className="middle line"></span>
+        <span className="bottom line"></span>
+      </button>
+      <nav className={hamburgerOpen ? "open" : null}>
+        <ul>
+          <li>
+            <Link
+              to="whiskey"
+              smooth={true}
+              offset={-100}
+              duration={750}
+              hashSpy={true}>
             Whiskey
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="tequila"
-            smooth={true}
-            offset={-100}
-            duration={750}
-            hashSpy={true}>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="tequila"
+              smooth={true}
+              offset={-100}
+              duration={750}
+              hashSpy={true}>
             Tequila
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="vodka"
-            smooth={true}
-            offset={-100}
-            duration={750}
-            hashSpy={true}>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="vodka"
+              smooth={true}
+              offset={-100}
+              duration={750}
+              hashSpy={true}>
             Vodka
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="gin"
-            smooth={true}
-            offset={-100}
-            duration={750}
-            hashSpy={true}>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="gin"
+              smooth={true}
+              offset={-100}
+              duration={750}
+              hashSpy={true}>
             Gin
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="rum"
-            smooth={true}
-            offset={-100}
-            duration={750}
-            hashSpy={true}>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="rum"
+              smooth={true}
+              offset={-100}
+              duration={750}
+              hashSpy={true}>
             Rum
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="tiki"
-            smooth={true}
-            offset={-100}
-            duration={750}
-            hashSpy={true}>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="tiki"
+              smooth={true}
+              offset={-100}
+              duration={750}
+              hashSpy={true}>
             Tiki
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="other"
-            smooth={true}
-            offset={-100}
-            duration={750}
-            hashSpy={true}>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="other"
+              smooth={true}
+              offset={-100}
+              duration={750}
+              hashSpy={true}>
             Other
-          </Link>
-        </li>
-      </ul>
-    </nav>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
