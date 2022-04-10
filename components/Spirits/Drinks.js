407cca52b1669f93cfd04868e6105642df53dce6
Chai Landau <clandau@cliquestudios.com>
2022-04-09 20:22:47 -0400

    integrating strapi api

--- components/Spirits/Drinks.js
+++ components/Spirits/Drinks.js
@@ -1,42 +1,11 @@
 import { DrinkList } from "../DrinkInfo/DrinkList";
-import dynamic from "next/dynamic";
 
 // ssr false so it only renders client side
-const Modal = dynamic( () => import( "../DrinkInfo/Modal" ), { ssr: false } );
 
 const Drinks = () => {
-  console.log( DrinkList );
+    // console.log( DrinkList );
 
-  return (
-    <>
-      {DrinkList.map( ( { spiritType, drinks } ) => {
-        return (
-          <div className="spirit-section" id={spiritType.toLowerCase()} key={spiritType.toLowerCase()}>
-            <div className="wrapper">
-              <div className="spirit-type">
-                <h2 className="spirit-title">{spiritType}</h2>
-                <div className="drinks">
-                  {drinks.map( ( { name, desc, recipe } ) => {
-                    return ( 
-                      <div className="drink-indiv" key={name.toLowerCase()}>
-                        <h3 className="drink-title">{name}</h3>
-                        <p>{desc}</p>
-                        <Modal 
-                          drinkName={name}
-                          recipe={recipe}
-                        />
-                      </div>
-                    );
-                  } )}
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      } )}
-    </>
-    
-  );
+    return <></>;
 };
 
 export default Drinks;
