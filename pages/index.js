eced3945f25fba20a8103926c2288687d563be96
Chai Landau <clandau@cliquestudios.com>
2022-04-09 20:23:00 -0400

    integrating strapi api

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,80 @@
-/* eslint-disable max-len */
-import Drinks from "../components/Spirits/Drinks";
+import axios from "axios";
+import { DrinkList } from "../components/DrinkInfo/DrinkList";
+import dynamic from "next/dynamic";
 import IconArrow from "../svg/IconArrow";
-let Scroll = require( "react-scroll" );
+let Scroll = require("react-scroll");
 let scroll = Scroll.animateScroll;
 
+const Modal = dynamic(() => import("../components/DrinkInfo/Modal"), { ssr: false });
+
 let scrollTop = () => {
-  // include this so it works with ssr
-  if ( typeof window !== "undefined" ) {
-    scroll.scrollToTop( { duration: 750 } );
-  }
+    // include this so it works with ssr
+    if (typeof window !== "undefined") {
+        scroll.scrollToTop({ duration: 750 });
+    }
 };
 
-const Home = () => {
-  return (
-    <div>
-      <Drinks />
-      <a onClick={scrollTop}>
-        <IconArrow />
-      </a>
-     
-    </div>
-  );
+const Home = ({ spirits }) => {
+    const spiritType = spirits.data;
+    console.log(spiritType);
+    return (
+        <div>
+            {DrinkList.map(({ spiritType, drinks }) => {
+                return (
+                    <div
+                        className="spirit-section"
+                        id={spiritType.toLowerCase()}
+                        key={spiritType.toLowerCase()}
+                    >
+                        <div className="wrapper">
+                            <div className="spirit-type">
+                                <h2 className="spirit-title">{spiritType}</h2>
+                                <div className="drinks">
+                                    {drinks.map(({ name, desc, recipe }) => {
+                                        return (
+                                            <div className="drink-indiv" key={name.toLowerCase()}>
+                                                <h3 className="drink-title">{name}</h3>
+                                                <p>{desc}</p>
+                                                <Modal drinkName={name} recipe={recipe} />
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+
+            {spiritType.map(({ attributes, id }) => {
+                return (
+                    <div
+                        className="spirit-section"
+                        id={attributes.Name.toLowerCase()}
+                        key={attributes.Name.toLowerCase()}
+                    >
+                        <div className="wrapper">
+                            <div className="spirit-type">
+                                <h2 className="spirit-title">{attributes.Name}</h2>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+            <a onClick={scrollTop}>
+                <IconArrow />
+            </a>
+        </div>
+    );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+    const spiritsRes = await axios.get("http://localhost:1337/api/spirits");
+    return {
+        props: {
+            spirits: spiritsRes.data,
+        },
+    };
+}
