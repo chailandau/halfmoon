525ae9eb0d95f5bb8c434501f194ed22626a49f8
Chai Landau <clandau@cliquestudios.com>
2022-04-15 13:43:57 -0400

    a11y work on modal -- adds esc close modal -- traps focus in modal -- reworks some event listeners to exterior callbacks
    a11y work on menu
    -- changes first menu item to button
    -- fixes styles for button
    -- makes sure everything tabbable
    
    adds mixin for underline on hover effect
    
    adds button animated hover

--- components/DrinkInfo/Modal.js
+++ components/DrinkInfo/Modal.js
@@ -1,19 +1,32 @@
 import { useState, useEffect } from "react";
 import IconClose from "../../svg/IconClose";
+import FocusTrap from "focus-trap-react";
 
 const Modal = (props) => {
     const [modal, setModal] = useState(false);
     const toggleModal = () => {
         setModal(!modal);
     };
+
     // add active-modal class if modal is open
     useEffect(() => {
         let html = document.querySelector("html");
+
+        const closeModalEsc = (e) => {
+            if (e.keyCode === 27) {
+                html.classList.remove("active-modal");
+                setModal(!modal);
+            }
+        };
+
         if (modal) {
             html.classList.add("active-modal");
+            // only run once when modal is open
+            document.addEventListener("keydown", closeModalEsc, { once: true });
         } else {
             html.classList.remove("active-modal");
         }
+        // close modal on esc
     }, [modal]);
 
     return (
@@ -25,49 +38,53 @@ const Modal = (props) => {
             {
                 // show modal if state is true
                 modal && (
-                    <div className="modal">
-                        <div className="overlay" onClick={toggleModal}>
-                            <div className="modal-content">
-                                <h2 className="drink-title">{props.Title}</h2>
-                                <ul>
-                                    {props.Ingredients.map((recipe, id) => {
-                                        return <li key={id}>{recipe.Ingredient}</li>;
-                                    })}
-                                </ul>
+                    <FocusTrap active={modal}>
+                        <div className="modal">
+                            <div className="overlay" onClick={toggleModal}>
+                                <div className="modal-content">
+                                    <h2 className="drink-title">{props.Title}</h2>
+                                    <ul>
+                                        {props.Ingredients.map((recipe, id) => {
+                                            return <li key={id}>{recipe.Ingredient}</li>;
+                                        })}
+                                    </ul>
 
-                                {
-                                    // show instructions only if they exist
-                                    props.Instructions.length != 0 && (
-                                        <div className="instructions">
-                                            <p>Instructions:</p>
-                                            <ul>
-                                                {props.Instructions.map((instructions, id) => {
-                                                    return <li key={id}>{instructions.Step}</li>;
-                                                })}
-                                            </ul>
-                                        </div>
-                                    )
-                                }
-                                {
-                                    // show optional only if it exists
-                                    props.Optional.length != 0 && (
-                                        <div className="optional">
-                                            <p>Optional:</p>
-                                            <ul>
-                                                {props.Optional.map((options, id) => {
-                                                    return <li key={id}>{options.Optional}</li>;
-                                                })}
-                                            </ul>
-                                        </div>
-                                    )
-                                }
+                                    {
+                                        // show instructions only if they exist
+                                        props.Instructions.length != 0 && (
+                                            <div className="instructions">
+                                                <p>Instructions:</p>
+                                                <ul>
+                                                    {props.Instructions.map((instructions, id) => {
+                                                        return (
+                                                            <li key={id}>{instructions.Step}</li>
+                                                        );
+                                                    })}
+                                                </ul>
+                                            </div>
+                                        )
+                                    }
+                                    {
+                                        // show optional only if it exists
+                                        props.Optional.length != 0 && (
+                                            <div className="optional">
+                                                <p>Optional:</p>
+                                                <ul>
+                                                    {props.Optional.map((options, id) => {
+                                                        return <li key={id}>{options.Optional}</li>;
+                                                    })}
+                                                </ul>
+                                            </div>
+                                        )
+                                    }
 
-                                <button className="close-modal" onClick={toggleModal}>
-                                    <IconClose />
-                                </button>
+                                    <button className="close-modal" onClick={toggleModal}>
+                                        <IconClose />
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </FocusTrap>
                 )
             }
         </div>
--- components/Header/Menu.js
+++ components/Header/Menu.js
@@ -1,122 +1,136 @@
 import { Link } from "react-scroll";
 import { useState, useEffect } from "react";
 
-const Scroll = require( "react-scroll" );
+const Scroll = require("react-scroll");
 const scroll = Scroll.animateScroll;
 
 const scrollTop = () => {
-  // include this so it works with ssr
-  if ( typeof window !== "undefined" ) {
-    scroll.scrollToTop( { duration: 750 } );
-  }
+    // include this so it works with ssr
+    if (typeof window !== "undefined") {
+        scroll.scrollToTop({ duration: 750 });
+    }
 };
 
 const Menu = () => {
-  const [ hamburgerOpen, setHamburgerOpen ] = useState( false );
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
-  const toggleHamburger = () => {
-    setHamburgerOpen( !hamburgerOpen );
-  };
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen);
+    };
 
-  useEffect( () => {
-    let html = document.querySelector( "html" );
-    let menuLinks = document.querySelectorAll( "header nav > ul > li > a" );
+    useEffect(() => {
+        let html = document.querySelector("html");
+        let menuLinks = document.querySelectorAll("header nav > ul > li > a");
+        let menuButton = document.querySelector("header nav > ul > li > button");
 
-    if ( hamburgerOpen ) {
-      html.classList.add( "nav-open" );
-      // close mobile menu if click on link
-      menuLinks.forEach( menuLink => {
-        menuLink.addEventListener( "click", ()=> {
-          html.classList.remove( "nav-open" );
-          // need to toggle state as well
-          setHamburgerOpen( !hamburgerOpen );
-        } );
-      } );
-    }
-    else {
-      html.classList.remove( "nav-open" );
-    }
-  }, [hamburgerOpen] );
+        // close menu when you click something on mobile
+        const closeMenu = () => {
+            html.classList.remove("nav-open");
+            // need to toggle state as well
+            setHamburgerOpen(!hamburgerOpen);
+        };
 
-  return (
-    <>
-      <button className="menu-toggle" onClick={toggleHamburger}>
-        <span className="top line"></span>
-        <span className="middle line"></span>
-        <span className="bottom line"></span>
-      </button>
-      <nav>
-        <ul>
-          <li>
-            <a 
-              onClick={scrollTop}>
-              Whiskey
-            </a>
-          </li>
-          <li>
-            <Link
-              to="tequila"
-              smooth={true}
-              offset={-100}
-              duration={750}
-              hashSpy={true}>
-            Tequila
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="vodka"
-              smooth={true}
-              offset={-100}
-              duration={750}
-              hashSpy={true}>
-            Vodka
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="gin"
-              smooth={true}
-              offset={-100}
-              duration={750}
-              hashSpy={true}>
-            Gin
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="rum"
-              smooth={true}
-              offset={-100}
-              duration={750}
-              hashSpy={true}>
-            Rum
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="tiki"
-              smooth={true}
-              offset={-100}
-              duration={750}
-              hashSpy={true}>
-            Tiki
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="other"
-              smooth={true}
-              offset={-100}
-              duration={750}
-              hashSpy={true}>
-            Other
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+        if (hamburgerOpen) {
+            html.classList.add("nav-open");
+            // close mobile menu if click on link
+            menuLinks.forEach((menuLink) => {
+                menuLink.addEventListener("click", closeMenu);
+            });
+            // add in for first button too (not an a tag)
+            menuButton.addEventListener("click", closeMenu);
+        } else {
+            html.classList.remove("nav-open");
+        }
+    }, [hamburgerOpen]);
+
+    return (
+        <>
+            <button className="menu-toggle" onClick={toggleHamburger}>
+                <span className="top line"></span>
+                <span className="middle line"></span>
+                <span className="bottom line"></span>
+            </button>
+            <nav>
+                <ul>
+                    <li>
+                        <button onClick={scrollTop}>Whiskey</button>
+                    </li>
+                    <li>
+                        <Link
+                            href={"#tequila"}
+                            to="tequila"
+                            smooth={true}
+                            offset={-100}
+                            duration={750}
+                            hashSpy={true}
+                        >
+                            Tequila
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={"#vodka"}
+                            to="vodka"
+                            smooth={true}
+                            offset={-100}
+                            duration={750}
+                            hashSpy={true}
+                        >
+                            Vodka
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={"#gin"}
+                            to="gin"
+                            smooth={true}
+                            offset={-100}
+                            duration={750}
+                            hashSpy={true}
+                        >
+                            Gin
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={"#rum"}
+                            to="rum"
+                            smooth={true}
+                            offset={-100}
+                            duration={750}
+                            hashSpy={true}
+                        >
+                            Rum
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={"#tiki"}
+                            to="tiki"
+                            smooth={true}
+                            offset={-100}
+                            duration={750}
+                            hashSpy={true}
+                        >
+                            Tiki
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href={"#other"}
+                            to="other"
+                            smooth={true}
+                            offset={-100}
+                            duration={750}
+                            hashSpy={true}
+                        >
+                            Other
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
 };
 
 export default Menu;
