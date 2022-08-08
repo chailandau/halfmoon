97715fc63561ca982399a97d7d3e4d54222ce657
Chai Landau <clandau@cliquestudios.com>
2022-08-08 11:51:44 -0400

    Adds current year variable to footer copyright

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,12 +1,15 @@
 const Footer = () => {
-  return (
-    <footer>
-      <div className="wrapper">
-        <p>Copyright © 2020 <span>Half Moon Tavern</span></p>
-        <p>Website by Chai Landau</p>
-      </div>
-    </footer>
-  );
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer>
+            <div className="wrapper">
+                <p>
+                    Copyright © {currentYear} <span>Half Moon Tavern</span>
+                </p>
+                <p>Website by Chai Landau</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
